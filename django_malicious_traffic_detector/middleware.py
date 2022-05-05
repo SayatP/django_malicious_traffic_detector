7199d4725c49c481bca3d97532eba8bcaab39ef6
Sayat Petrosyan <sayatp98@gmail.com>
2022-05-05 22:09:23 +0400

    configurable data and treshold

--- django_malicious_traffic_detector/middleware.py
+++ django_malicious_traffic_detector/middleware.py
@@ -9,9 +9,15 @@ from .model import MaliciousTrafficModelProxy
 
 class MaliciousTrafficMiddleware:
     def __init__(self, get_response):
+
+        settings = settings.DJANGO_MALICIOUS_TRAFFIC_DETECTOR
+        self.treshold = settings["TRESHOLD"]
         self.get_response = get_response
         self.datastore = MalicilousTrafficDataStore()
-        self.model = MaliciousTrafficModelProxy()
+        self.model = MaliciousTrafficModelProxy(
+            user_agents_file=settings["USER_AGENTS_FILE"],
+            queries_file=settings["QUERIES_FILE"],
+        )
 
     def __call__(self, request):
 
@@ -20,13 +26,13 @@ class MaliciousTrafficMiddleware:
         ip = self.get_client_ip(request)
         frequency = self.datastore.get_number_of_requests_from_ip(ip)
 
-        is_malicious = self.model.predict(
+        malicious_prediction = self.model.predict(
             frequency=frequency,
             user_agent=user_agent,
             query=query_params,
         )
 
-        if not is_malicious:
+        if malicious_prediction < self.treshold:
             response = self.get_response(request)
 
         else:
